b063b4ab4152fc23b06552356c6b4978441c6689
alessandro.rosa <alessandrorosa87@gmail.com>
2020-05-05 21:19:47 +0200

    added changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Cool breakfast`,
-    description: `I am a software engineer focusing on back-end development. Always looking for cool stuff to talk about`,
+    description: `I am a software engineer focusing on web technologies. Always looking for cool stuff to talk about`,
     author: `Alessandro Rosà`,
     siteUrl: `https://www.alessandrorosa.com`,
     social: {
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -14,7 +14,7 @@ const Sidebar = ({ siteMetadata }) => (
             </Link>
           </div>
           <div className="author-name">{siteMetadata.author}</div>
-          <p>{siteMetadata.description}</p>
+          <p className="author-description"> {siteMetadata.description}</p>
           <div className="link-sidebar">
             <Link to="/about">ABOUT</Link>
           </div>
